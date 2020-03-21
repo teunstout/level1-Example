8664bd6d99ae34e40c6bcfdf9ddfda4be055a0cc
TeunStout <teunstout@hotmail.com>
2020-03-21 20:22:44 +0100

    Comment

--- Level1Example/app/src/main/java/com/example/example1/MainActivity.kt
+++ Level1Example/app/src/main/java/com/example/example1/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.example1
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -12,21 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnConfirm.setOnClickListener {
-            checkAwnser()
-        }
+        btnConfirm.setOnClickListener { checkAwnser() }
     }
 
     fun checkAwnser() {
+        // To lower case so Giraffe and giraffe are both good
         val awnser =
             if (getString(R.string.correctAwnser).toLowerCase() == etAnswer.text.toString().toLowerCase()) {
                 R.string.correct
             } else {
                 R.string.incorrect
             }
-
-
-
         Toast.makeText(this, awnser, Toast.LENGTH_SHORT).show()
     }
 }
